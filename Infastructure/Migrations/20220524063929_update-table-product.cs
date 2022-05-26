186b9315ef12698d0849bb6a30bbcdf59df1fda9
tieplk <tieplk@dev.ndex.net>
2022-05-26 08:54:54 +0700

    file migrate update table

---
+++ Infastructure/Migrations/20220524063929_update-table-product.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infastructure.Migrations
+{
+    public partial class updatetableproduct : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SeoDescription",
+                table: "Products",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SeoTitle",
+                table: "Products",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SeoDescription",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "SeoTitle",
+                table: "Products");
+        }
+    }
+}
